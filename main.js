c9230ed14bf886ebd92eb3d600a10e2f7f05c449
jorgeayala1984 <jorgeayalaelectricista@gmail.com>
2023-06-15 15:57:10 -0300

    correccion

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const inputTexto = document.querySelector('#input-texto');
 const inputTexto1 = document.querySelector('#input-texto1');
 const inputpedido = document.querySelector('#inputpedido');
 const parrafo = document.querySelector('#parrafo');
+const nopendientes =document.querySelector("#empy");
 
 //funcion concatenar nombre y pedido
 function mostrar() {
@@ -51,17 +52,20 @@ const p_tarea_nueva = document.querySelector('#p_tarea')
 const ul_tarea_nueva = document.querySelector('#ul_tareas')
 
 boton_agregar_tarea.addEventListener('click', (e) => {
-    console.log(1)
+    
     e.preventDefault();//para q no se recarge la pagina
 
     const texto_input = nuevaTarea.value;//valor del input(nuevatarea)
     const li =document.createElement('li');//crea un elemento nuevo el la lista
+    li.classList.add("horizontal")
     const p = document.createElement('p');//crea el parrafo dentro de esa lista
     p.textContent = texto_input;//el texto parrafo es el del texto input
     li.appendChild(p);//unir el li con el parrafo
     ul_tarea_nueva.appendChild(li);
-
-
+    nuevaTarea.value = "";//borra la tareapendiente anterior
+    nopendientes.style.display = "none";//oculta el div con el parrafo de "no tenes pendientes"
+    const Btn =document.createElement('button');
+    li.appendChild(Btn);
 })
 
 function addDeleteBtn () {
